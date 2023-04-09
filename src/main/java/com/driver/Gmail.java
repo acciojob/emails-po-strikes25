42ecba6e7a084619bd5e483443079b9d3ec3147b
Shilpi <svvar31@gmail.com>
2023-04-09 18:18:18 +0530

    Removed <=

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -39,7 +39,7 @@ public class Gmail extends Email {
 
         int rotation_count = 0;
         // Included <= in the for loop :
-        for(int i = 0 ; i <= inbox_size ; i++) {
+        for(int i = 0 ; i < inbox_size ; i++) {
             if(this.inbox.peek().message.equals(message)) {
                 this.trash.offer(this.inbox.poll());
                 rotation_count = i;
